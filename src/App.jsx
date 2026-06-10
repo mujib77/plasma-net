724d6d0b4d35eb27f8ae061398399998738b1b73
mujib77 <mujibkhanpathan6@gmail.com>
2026-06-10 17:19:13 +0530

    feat(canvas): render solar wind particles between sun and earth

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,17 @@ import Earth from './components/Earth'
 import ISS from './components/ISS'
 import HUD from './components/HUD'
 import StarField from './components/StarField'
+import useSolarWind from './hooks/useSolarWind'
+import SolarWind from './components/Particles'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
 
 export default function App() {
   const [issData, setIssData] = useState(null) 
+  const solarWind = useSolarWind() 
 
   return (
     <div style={{ width: '100vw', height: '100vh', background: '#000' }}>
-      <HUD issData={issData} />  
+      <HUD issData={issData} solarWind={solarWind} />  
       <Canvas camera={{ position: [0, 0, 10], fov: 50 }}
         style={{ position: 'absolute', top: 0, left: 0 }}>
         <ambientLight intensity={0.5} />
@@ -26,6 +29,7 @@ export default function App() {
         <Suspense fallback={null}>
           <Earth position={[3.5, 0, 0]} />
         </Suspense>
+        <SolarWind solarWind={solarWind} sunPosition={[-3, 0, 0]} earthPosition={[3.5, 0, 0]} />
         <ISS earthPosition={[3.5, 0, 0]} onData={setIssData} /> 
         <EffectComposer>
           <Bloom intensity={1.5} luminanceThreshold={0.3} luminanceSmoothing={0.9} />
