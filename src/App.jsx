7f2e450b82d97420beb50a9b2c9ed38879081b01
mujib77 <mujibkhanpathan6@gmail.com>
2026-06-11 12:17:18 +0530

    feat(app): add Asteroids component and state tracking

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,17 @@ import HUD from './components/HUD'
 import StarField from './components/StarField'
 import useSolarWind from './hooks/useSolarWind'
 import SolarWind from './components/Particles'
+import Asteroids from './components/Asteroids'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
 
 export default function App() {
   const [issData, setIssData] = useState(null) 
   const solarWind = useSolarWind() 
+  const [asteroidsData, setAsteroidsData] = useState(null)
 
   return (
     <div style={{ width: '100vw', height: '100vh', background: '#000' }}>
-      <HUD issData={issData} solarWind={solarWind} />  
+      <HUD issData={issData} solarWind={solarWind} asteroidsData={asteroidsData} />  
       <Canvas camera={{ position: [0, 0, 10], fov: 50 }}
         style={{ position: 'absolute', top: 0, left: 0 }}>
         <ambientLight intensity={0.5} />
@@ -31,6 +33,7 @@ export default function App() {
         </Suspense>
         <SolarWind solarWind={solarWind} sunPosition={[-3, 0, 0]} earthPosition={[3.5, 0, 0]} />
         <ISS earthPosition={[3.5, 0, 0]} onData={setIssData} /> 
+        <Asteroids earthPosition={[3.5, 0, 0]} onData={setAsteroidsData} />
         <EffectComposer>
           <Bloom intensity={1.5} luminanceThreshold={0.3} luminanceSmoothing={0.9} />
         </EffectComposer>
